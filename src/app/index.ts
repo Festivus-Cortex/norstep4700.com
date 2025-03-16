e54832e75acba6fd3a31502aad49ac68c565ce4e
Preston <norstep4700@gmail.com>
2025-03-16 19:19:38 -0400

    chore: remove unused pdf testing file

--- src/app/index.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { pdfjs } from "react-pdf";
-
-console.log("here index");
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
