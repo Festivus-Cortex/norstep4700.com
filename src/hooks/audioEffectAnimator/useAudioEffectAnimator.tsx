b7aedf73436b34af08f1f1c4c1a430d8dabbba42
Preston <norstep4700@gmail.com>
2025-03-23 20:48:56 -0400

    chore: correct lint options and copy script

--- src/hooks/audioEffectAnimator/useAudioEffectAnimator.tsx
+++ src/hooks/audioEffectAnimator/useAudioEffectAnimator.tsx
@@ -6,11 +6,11 @@ import { AudioEffectAnimatorConfig } from "./audioEffectAnimatorConfig";
 function useAudioEffectAnimator(config: AudioEffectAnimatorConfig) {
   const [animatorEnabled, setAnimatorEnabled] = useState(true);
   const updateAnimation = (currentTime: number) => {
-    console.log(
+    /*console.log(
       "updateAnimation - animatorEnabled is %s and currentTime is %d",
       animatorEnabled.toString(),
       currentTime
-    );
+    );*/
     if (!animatorEnabled) {
       return;
     }
